707c0d38f14a4b4aa209e2ea196a1a67274f3d11
Tishaun Bristol <tishaun@tishauns-mbp.myfiosgateway.com>
2022-08-23 14:35:08 -0400

    Added POST METHOD

--- books.py
+++ books.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -19,11 +20,6 @@ class DirectionName(str, Enum):
     west = "West"
 
 
-@app.get('/')
-async def read_all_book():
-    return BOOKS
-
-
 @app.get('/books/mybook')
 async def read_favorite_book():
     return {'book_title': 'My favorite book '}
@@ -33,17 +29,41 @@ async def read_favorite_book():
 async def read_book(book_id: int):
     return {'book_id': book_id}
 
+
 @app.get('directions/{direction_name}')
-async def get_direction(direction_name:DirectionName):
+async def get_direction(direction_name: DirectionName):
     if direction_name == DirectionName.north:
-        return{'Direction': direction_name, 'sub':'up'}
+        return {'Direction': direction_name, 'sub': 'up'}
     if direction_name == DirectionName.south:
-        return{'Direction': direction_name, 'sub':'down'}
+        return {'Direction': direction_name, 'sub': 'down'}
     if direction_name == DirectionName.east:
-        return{'Direction': direction_name, 'sub':'left'}
+        return {'Direction': direction_name, 'sub': 'left'}
     if direction_name == DirectionName.west:
-        return{'Direction': direction_name, 'sub':'right'}
-    
+        return {'Direction': direction_name, 'sub': 'right'}
+
+
+@app.get('/')
+async def read_all_book(skip_book: Optional[str] = None):
+    if skip_book:
+        new_books = BOOKS.copy()
+        del new_books[skip_book]
+        return new_books
+    return BOOKS
+
+
 @app.get("/book_name}")
-async def read_book(book_name:str):
+async def read_book(book_name: str):
     return BOOKS[book_name]
+
+
+@app.post("/")
+async def create_book(book_title, book_author):
+    current_book_id = 0
+    if len(BOOKS) > 0:
+        for books in BOOKS:
+            x = int(books.split('_')[-1])
+            if x > current_book_id:
+                current_book_id = x
+    BOOKS[f'books_{current_book_id+1}'] = {'title': book_title,
+                                           'author': book_author}
+    return BOOKS[f'book_{current_book_id+1}']
